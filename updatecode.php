fd591c7bdaafc9b841793cf86935890c54634c74
Himanshu Rathore <36191619+himanshu-802@users.noreply.github.com>
2020-05-29 10:06:56 +0530

    Update updatecode.php
    Updated

--- updatecode.php
+++ updatecode.php
@@ -1,24 +1,36 @@
 <?php
-$connection = mysqli_connect("localhost","root","","phpcrud");
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "rentomojo";
+$connection =mysqli_connect($servername, $username, $password, $dbname);
 
 if(isset($_POST['updatedata']))
 {
 
-	$fname = $_POST['fname'];
-	$lname = $_POST['lname'];
-	$course = $_POST['course'];
+    
+	$fname=$_POST['fname'];
+	$dob=  $_POST['dob'];
+	$contact1=$_POST['contact1'];
+	$contact2=$_POST['contact2'];
+	$email=$_POST['email'];
 
-	$query = "UPDATE student SET fname='$fname' ,lname ='$lname' , course = '$course' WHERE fname='$fname'  ";
-	$query_run= mysqli_query($connection, $query);
-
+	
+	$query="UPDATE `phonebook` SET `fname`='$fname',`dob`='$dob',`contact1`='$contact1',`contact2`='$contact2',`email`='$email' WHERE contact1='$contact1' ";
+	$query_run=mysqli_query($connection,$query);
 
 	if($query_run){
-        	
-		echo '<script> alert("Data Updated"); </script> ';
+		
+		echo '<script> alert("Data Saved"); </script>';
 		header('Location:index.php');
 	}
 	else{
 		echo '<script> alert("Data Not Saved"); </script>';
 	}
+
 }
+else{
+	echo "not connected";
+}
+
 ?>
