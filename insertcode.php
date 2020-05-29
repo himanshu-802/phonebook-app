eff1aca1335e8b936141a7ddfa68bf7ddf07b371
Himanshu Rathore <36191619+himanshu-802@users.noreply.github.com>
2020-05-29 10:06:16 +0530

    Update insertcode.php
    Updated

--- insertcode.php
+++ insertcode.php
@@ -2,27 +2,23 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "phpcrud";
+$dbname = "rentomojo";
 $connection =mysqli_connect($servername, $username, $password, $dbname);
 
 if(isset($_POST['insertdata']))
 {
-	// $name= $_POST['name'];
- //    $dob= $_POST['dob'];
- //    $contact1= $_POST['contact1'];
- //    $contact2= $_POST['contact2'];
-	// $email1= $_POST['email1'];
-	// $email2= $_POST['email2'];
-
-	// $query="INSERT INTO `phonebook`(`name`, `dob`, `contact1`, `contact2`, `email1`, `email2`) VALUES ('$name','dob','contact1','contact2','email1','email2')";
-
 
 	$fname=$_POST['fname'];
-	$lname=$_POST['lname'];
-	$course=$_POST['course'];
-	$query="INSERT INTO `student`(`fname`, `lname`, `course`) VALUES ('$fname','$lname','$course')";
+	$dob=$_POST['dob'];
+	$contact1=$_POST['contact1'];
+	$contact2=$_POST['contact2'];
+	$email=$_POST['email'];
+
+	echo $fname;
+	
+
+	$query="INSERT INTO `phonebook`(`fname`, `dob`, `contact1`, `contact2`, `email`) VALUES ('$fname','$dob','$contact1','$contact2','$email')";
 	$query_run=mysqli_query($connection,$query);
-	// $query_run=mysqli_query($connection,$query);
 
 	if($query_run){
 		
